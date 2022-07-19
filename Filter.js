3e30980cb1583f9326d4e39f32642ebc59fd1501
komali <Komalikoms69@gmail.com>
2022-07-19 12:02:07 +0530

    initial commit

---
+++ Filter.js
@@ -0,0 +1,33 @@
+
+import React,{useState} from 'react';
+
+function Filter(){
+    const fruits=['Apple','Banana','Pineapple','Orange'];
+    const[list,setList]=useState(fruits);
+
+    const handleSearch=(event)=>{
+          
+        if(event.target.value===''){
+            setList(fruits);
+            return;
+        }
+
+        const filteredList=fruits.filter(item=>item.toLowerCase().indexOf(event.target.value.toLowerCase())!= -1);
+        setList(filteredList);
+    }
+    
+       
+
+    return(
+        <div>
+        <div>
+            Search: <input type='text' onChange={handleSearch} />
+        </div> 
+        { list && list.map((item,index)=>(
+            <div key={index}> {item} </div>
+        
+        ))}
+        </div>
+    )
+}
+export default Filter;
